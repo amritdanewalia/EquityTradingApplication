58acbe997a2331825f156f0314bf3b62bb06ae64
Amritpal Danewalia <adanewalia@sapient.com>
2017-07-18 18:00:24 +0530

    Code clean up

--- EquityTradingApplication/EquityTradingApplicationCore/src/main/java/com/sadp/equity/trading/application/controlller/UserBean.java
+++ EquityTradingApplication/EquityTradingApplicationCore/src/main/java/com/sadp/equity/trading/application/controlller/UserBean.java
@@ -1,15 +1,17 @@
 package com.sadp.equity.trading.application.controlller;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Arrays;
+
 public class UserBean {
 
-    private String userId;
-    private char[] password;
+    private final String userId;
+    private final char[] password;
 
-    public UserBean() {
-
-    }
-
-    public UserBean(String userId, char[] password) {
+    @JsonCreator
+    public UserBean(@JsonProperty("userId") String userId, @JsonProperty("password") char[] password) {
         this.userId = userId;
         this.password = password;
     }
@@ -22,5 +24,30 @@ public class UserBean {
         return password;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        UserBean userBean = (UserBean) o;
+
+        if (userId != null ? !userId.equals(userBean.userId) : userBean.userId != null) return false;
+        return Arrays.equals(password, userBean.password);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + Arrays.hashCode(password);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserBean{" +
+                "userId='" + userId + '\'' +
+                ", password=" + Arrays.toString(password) +
+                '}';
+    }
 }
